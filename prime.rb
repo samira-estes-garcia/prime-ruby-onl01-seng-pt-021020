b6cd493e47831a5737b6b5527ad1d83ec1a646d6
Samira Estes-Garcia <samira.estes.garcia@gmail.com>
2020-02-20 03:32:40 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -2,8 +2,8 @@ def prime?(int)
   if int < 0 || int == 0 || int == 1
     return false
   else
-   (2..num-1).to_a.all? do |possible_factor|
-      num % possible_factor != 0
+   (2..int-1).to_a.all? do |possible_factor|
+      int % possible_factor != 0
     end
   end
 end
